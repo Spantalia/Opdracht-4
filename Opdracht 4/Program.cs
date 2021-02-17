7aaafab5b7e65391aa926fec022b8b09e166bcd4
Spantalia <0322213@student.rocvantwente.nl>
2021-02-17 09:42:40 +0100

    Add project files.

---
+++ Opdracht 4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Opdracht_4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[] array1 = new int[10];
+            int[] array2 = new int[10];
+            int[] array3 = new int[10];
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = GenerateRandomNumber(rnd);
+                array2[i] = GenerateRandomNumber(rnd);
+                array3[i] = array1[i] + array2[i];
+            }
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write(array1[i] + "\t");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write(array2[i] + "\t");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array3.Length; i++)
+            {
+                Console.Write(array3[i] + "\t");
+            }
+        }
+        //returnType MethodeNaam(param1, param2)
+        private static int GenerateRandomNumber(Random rnd)
+        {
+            return rnd.Next(10, 99);
+        }
+
+    }
+}
